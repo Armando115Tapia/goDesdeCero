ac93d6b86d16fe20de2f2e9c747d861423f708b5
armandotapiach <armando.tapia@kushkipagos.com>
2024-02-06 10:46:48 -0500

    [GO] if and switch structure

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/Armando115Tapia/goDesdeCero/variables"
+	"runtime"
 )
 
 func main() {
@@ -10,4 +11,18 @@ func main() {
 	estado, valor := variables.ConviertoaTexto(14)
 	fmt.Println(estado)
 	fmt.Println(valor)
+
+	if os := runtime.GOOS; os == "darwin" {
+
+		fmt.Println("Loco este Go, ya vamos a cachar las gorutinas")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("Este de mac")
+	case "Linux.":
+		fmt.Println("Este es de Linux")
+
+	}
+
 }
